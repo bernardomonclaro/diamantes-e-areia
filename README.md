6c3194062ef0c4d98cbcf3367ed7605ce62675d6
Bernardo Lima <bernardomonclaro@gmail.com>
2022-07-06 12:39:02 -0300

    ajustes finais

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 Desafio Lógica de programação
 
-Dado um conjunto de caracteres, você precisará extrair os "diamantes" ( <>) e as "areias" ( . ) da expressão e no final exibir a quantidade de diamantes extraídos
+Dado um conjunto de caracteres, você precisará extrair os "diamantes" ( <> ) e as "areias" ( . ) da expressão e no final exibir a quantidade de diamantes extraídos
 Expressão: <<.<<..>><>><.>.>.<<.>.<.>>>><>><>>
 
 Requisitos:
-● Extrair os diamantes e areias da expressão até que não haja mais o que ser extraído;
-● Exibir a quantidade de diamantes extraídos;;
-● O projeto deve ser disponibilizado em um repositório aberto no GitHub. Envie a URL assim que
+* Extrair os diamantes e areias da expressão até que não haja mais o que ser extraído;
+* Exibir a quantidade de diamantes extraídos;;
+* O projeto deve ser disponibilizado em um repositório aberto no GitHub. Envie a URL assim que
 possível.
--- desafio2.rb
+++ desafio2.rb
@@ -1,10 +1,9 @@
-expressao = "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>"
-contador = 0 # contador para os diamantes encontrados.
-exp = expressao.tr('.', '') # cria uma nova string, trocando o . por string vazia, ou seja, removendo o .
-exp.tap do |expressao_sem_areia| # o tap retorna a própria string sem o . anterior como objeto, ficando disponível para ser usado dentro do bloco
-  while expressao_sem_areia.sub!(/<>/, '')
-    puts expressao_sem_areia  # dentro do bloco é iniciado a iteração no objeto usando um while, e caso seja encontrado o diamante '<>' ele mostra o próprio objeto com o diamante extraído e o contador para mostrar que foi encontrado e incrementado
-    contador += 1
-  end
+expressao = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
+contador = 0 # Contador para os diamantes encontrados.
+x = expressao.tr('.', '') # Cria uma nova string, trocando o '.' por string vazia, ou seja, removendo o '.'
+while x.sub!(/<>/, '') # Dentro do bloco é iniciado a iteração no objeto usando um while.
+  puts x               # Caso seja encontrado o diamante '<>', ele mostra o próprio objeto com o diamante extraído.
+  contador += 1        # A cada diamante retirado da expressão, o contador incrementa com + 1.
 end
-puts "Foram encontrados #{contador} diamantes"
+
+puts "Foram encontrados #{contador} diamantes" # Por fim ele printa na tela a quantidade de diamantes encontrados.
